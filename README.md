06f5c89aa937e9ea7df7ed6a4507b56a40ffd099
v-aguiar <v.aguiar.dev@gmail.com>
2021-12-07 21:25:12 -0300

    First commit :tada:

---
+++ README.md
@@ -0,0 +1,21 @@
+## Desafio 1 - Conceitos de Node JS
+
+### Requests
+
+- [x] It must be possible to create a new user with `name` and `username`
+- [] It must be possible to list all tasks
+- [] It must be possible to create a new task
+- [] It must be possible to change `title` and `deadline` from a existing task
+- [] It must be possible to check a task as done
+- [] It must be possible to delete a task
+- [] It must be possible to CRUD tasks
+
+### Bussiness Rules
+
+- [x] The `username` must be passed through the header
+- []
+- []
+- []
+- []
+- []
+- []
--- src/index.js
+++ src/index.js
@@ -1,21 +1,54 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
-function checksExistsUserAccount(request, response, next) {
+// Middleware responsible for checking if account exists
+function checksExistsUserAccount( request, response, next ) {
   // Complete aqui
+
+  const { username } = request.headers
+
+  const user = users.filter( user => { user.username === username })
+
+  if( !user ) {
+    return response.status(400).json({error: "User doesn`t exists!"})
+  }
+
+  request.user = user
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
   // Complete aqui
+  const { name, username } = request.body
+
+  const userExists = (users.some( user => user.username === username ))
+
+  if( userExists ) {
+    return response.status(400).json({ error: "User already exists!" })
+  }
+
+  users.push({
+    name,
+    username,
+    id: uuidv4(),
+    todo: []
+  })
+
+  let user = []
+
+  users.forEach( u => { (u.username === username) ? user.push(u) : [] })
+
+  return response.status(200).json(user)
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
